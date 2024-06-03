e14cdb8f04d54b0b0009c7f5ae8015b5b43c7882
enowdivine <sirdivine16@gmail.com>
2024-06-03 12:27:09 +0100

    setup email service function

---
+++ src/services/email/email.ts
@@ -0,0 +1,53 @@
+import nodemailer from "nodemailer";
+import dotenv from "dotenv";
+import email from "./templates/template";
+
+dotenv.config();
+
+interface Option {
+  from?: string;
+  to: string;
+  subject: string;
+  title: string;
+  message: string;
+}
+
+export default function mailer(option: Option): void {
+  const html: string = email(option.title, option.message);
+  const transporter: any = nodemailer.createTransport({
+    // host: process.env.EMAIL_HOST,
+    // port: process.env.EMAIL_PORT,
+    // secure: true, // Use SSL
+    service: process.env.EMAIL_SERVICE,
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_USER_PASSWORD,
+    },
+    tls: {
+      rejectUnauthorized: false,
+    },
+  } as any);
+  const mailOptions: any = {
+    sender: "Campus Camer Inc",
+    from: option.from || `Campus Camer Inc <noreply@campus-camer.com>`,
+    to: option.to,
+    bcc: "everything@campus-camer.com",
+    subject: option.subject,
+    html: html,
+  };
+  try {
+    const result = transporter.sendMail(mailOptions);
+    transporter.close();
+    return result;
+  } catch (error) {
+    console.error(error);
+    return;
+  }
+}
+
+mailer({
+  to: "sirdivine16@gmail.com",
+  subject: `Welcome`,
+  title: "title",
+  message: `<h1>Hello</h1>`,
+});
