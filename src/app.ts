a07927f35572afa64b948b591523a76a6e3903c9
enowdivine <sirdivine16@gmail.com>
2024-07-26 12:30:30 +0100

    update

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from "express";
 import bodyParser = require("body-parser");
 import dbConnect from "./config/db";
-import dotenv from "dotenv";
+// import dotenv from "dotenv";
 import http from "http";
 import cors from "cors";
 // api imports
@@ -22,7 +22,7 @@ const corsOptions = {
   optionSuccessStatus: 200,
 };
 
-dotenv.config();
+require("dotenv").config();
 const app = express();
 const server: any = http.createServer(app);
 
