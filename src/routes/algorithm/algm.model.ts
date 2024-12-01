1c8c56d19e89c11901cfc7f814724174a72cdb17
enowdivine <sirdivine16@gmail.com>
2024-12-01 09:23:04 +0100

    user interaction model

--- src/routes/algorithm/algm.model.ts
+++ src/routes/algorithm/algm.model.ts
@@ -5,9 +5,6 @@ const userMatchSchema = new mongoose.Schema(
     user1: { type: String, ref: "User", required: true },
     user2: { type: String, ref: "User", required: true },
     score: { type: Number, required: true },
-    user1Liked: { type: Boolean, default: false },
-    user2Liked: { type: Boolean, default: false },
-    isMutual: { type: Boolean, default: false },
   },
   {
     timestamps: true,
@@ -20,8 +17,6 @@ userMatchSchema.index({ user2: 1 });
 userMatchSchema.index({ user1: 1, user2: 1 }, { unique: true }); // Unique compound index for efficient lookups between two users
 userMatchSchema.index({ score: -1 }); // Index for sorting by score in descending order
 userMatchSchema.index({ timestamp: -1 }); // Index for queries based on the timestamp
-userMatchSchema.index({ isMutual: 1 }); // Index for filtering mutual matches
-userMatchSchema.index({ user1Liked: 1, user2Liked: 1 }); // Compound index for filtering based on like status
 userMatchSchema.index({ user1: 1, score: -1 }); // Index for efficient queries involving user1 and score sorting
 userMatchSchema.index({ user2: 1, score: -1 }); // Index for efficient queries involving user2 and score sorting
 
---
+++ src/routes/user/user.interactionModel.ts
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const userInteractionSchema = new mongoose.Schema(
+  {
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true }, // The user performing the action
+    targetUser: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    }, // The user being liked/disliked
+    type: { type: String, enum: ["LIKE", "DISLIKE"], required: true }, // Interaction type
+  },
+  {
+    timestamps: true, // Includes `createdAt` and `updatedAt` fields
+  }
+);
+
+// Indexing
+userInteractionSchema.index({ user: 1, targetUser: 1 }, { unique: true }); // Ensure one interaction per pair of users
+userInteractionSchema.index({ type: 1 }); // Query by interaction type (like/dislike)
+userInteractionSchema.index({ createdAt: 1 }); // Sort by interaction time
+
+export default mongoose.model("UserInteraction", userInteractionSchema);
